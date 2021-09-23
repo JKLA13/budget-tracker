735a447f11607515322dc4c6ee838a2075fc31b4
Jacob Adamson <jakeladamson@gmail.com>
2021-09-22 20:02:52 -0600

    worked code for service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,83 @@
+//variable to store an array of strings and represents the files we want available offline
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/favicon.ico",
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-512x512.png",
+];
+// variable using to store the names of our cache, can be named whatever
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install  - cache assets
+self.addEventListener("install", function (evt) {
+  //1 open a cache, 2 actually cache files
+  //evt.waitUntil takes a promise
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+//manage old caches after activating the service worker
+self.addEventListener("activate", function (evt) {
+  //returns a promise that resolves to an array of cache keys, returned the same order as they are inserted
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        //mapping over the array of cache keys, if the key is not equal to cachename and we remove the old cache
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
